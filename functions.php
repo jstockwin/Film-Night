836c45acde56dbe9a1ae2277e1723e745dcd83fe
jstockwin <jstockwin@gmail.com>
2016-01-24 19:27:33 +0000

    Set up local dev environment (always logged in)

--- functions.php
+++ functions.php
@@ -1,12 +1,20 @@
 <?php
 
-  function loginCheck() {
-    session_start();
+function loginCheck($session = "live") {
+  session_start();
+  if($session == "dev"){
+    $_SESSION['email'] = "debug@example.com";
+    $_SESSION['Permission'] = "admin";
+    $_SESSION['Name'] = "debug";
+    $_SESSION['Token'] = "ABC123";
+    return "admin";
+  }else{
     if (isset($_SESSION['Permission'])){
-    	return $_SESSION['Permission'];
+      return $_SESSION['Permission'];
     }else{
-    	return FALSE;
+      return FALSE;
     }
   }
+}
 
- ?>
+?>
