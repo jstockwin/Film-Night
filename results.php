8d2431ecb879ea9f97da1045b4666617f0d397d2
Tom Johnston <tom.johnston5861@gmail.com>
2016-01-25 16:53:40 +0000

    Initial Commit

---
+++ results.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+  <script type="text/javascript" src="voting-systems.js"></script>
+  <div id="header">
+  </div>
+  <div id="svg-container">
+    <svg
+    id="icon"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="#fff"
+    viewBox="0 0 24 24"
+    style="overflow: visible">
+    <path
+    d="M 2.005,8 2,18 c 0,1.1 0.9,2 2,2 l 16,0 c 1.1,0 2,-0.9 2,-2 L 22,8 z"
+    id="bottom" />
+    <path
+    d="M0 0h24v24H0z"
+    fill="none"
+    id="border" />
+    <path
+    id="top"
+    d="M 17.999,4 19.9995,8 16.99875,8 14.99825,4 12.997749,4 14.99825,8 11.997499,8 9.9969992,4 7.9964991,4 9.9969992,8 6.9962491,8 4.9957489,4 3.9954989,4 C 2.8952238,4 2.0050013,4.9 2.0050013,6 L 2,8 22,8 22,4 z" />
+  </svg>
+</div>
+<p id="log"></p>
+<div id="container">
+<div id="results">
+  <div id="winner-div">
+    <h3>This week's winner is</h3>
+    <h2 id="winner">TODO</h2>
+  </div>
+  <div id="utilities">
+    <div id="distances"><table id="distances-table"></table></div>
+    <div id="svg">
+
+      </defs>
+    </svg>
+  </div>
+  <div id="tables">
+    <div id="schulze"></div>
+    <div id="copeland"></div>
+    <div id="borda"></div>
+    <div id="minimax"></div>
+    <div id="kemenyYoung"></div>
+    <div id="baldwin"></div>
+  </div>
+</div>
+</div>
+<script>
+
+function closeClapper(){
+  document.getElementById('top').style.transform = "rotate(0deg)";
+}
+
+function shrinkHeader(){
+  document.getElementById('svg-container').style.left = "0";
+  document.getElementById('svg-container').style.top = "0";
+  document.getElementById('svg-container').style.transform = "scale(calc(70 / 300))";
+  document.getElementById('header').style.height = "70px";
+}
+
+function hideLog(){
+  document.getElementById('log').style.visibility = "hidden";
+}
+
+var line1 = "";
+var line2 = "";
+var line3 = "";
+function log(string, update){
+  var log = document.getElementById('log');
+  if(update !== true){
+    line1 = line2;
+    line2 = line3;
+    line3 = string;
+  }else{
+    line3 = string;
+  }
+  log.innerHTML = line1 + "<br>" + line2 + "<br>" + line3;
+  console.log(string);
+}
+
+var results= [];
+
+window.onload = runAllAlgorithms(1);
+
+function createInformationOnAlgorithm(algorithmName, algorithm, listOfCandidates, votes, divToPopulate, onFinish){
+  var start = performance.now();
+  divToPopulate.classList.add('algorithm');
+  divToPopulate.innerHTML = '<h2>' + algorithmName + '</h2> <h3 id="' + algorithmName + 'Time" class="time"></h3>';
+  try{
+    var result = algorithm(listOfCandidates, votes);
+    results.push(result);
+    divToPopulate.innerHTML = divToPopulate.innerHTML + '<table class="results-table" id="' + algorithmName + 'Table">' + populateTable(result) + '</table>';
+  }catch(err){
+    divToPopulate.innerHTML = divToPopulate.innerHTML + '<p class="error-message">'+err+'</p>';
+  }
+  var timeTaken  = Math.round(performance.now() - start)/1000;
+  document.getElementById(algorithmName + 'Time').innerHTML = timeTaken + 's';
+  log(algorithmName + '...' + timeTaken + 's', true);
+  onFinish();
+}
+
+function runAllAlgorithms(i) {
+  if( i === 1){
+    generateRandomVotes(1000);
+    log('Schulze Ranking...');
+    setTimeout(createInformationOnAlgorithm,0,'Schulze', schulze, listOfCandidates, votes, document.getElementById('schulze'),function(){runAllAlgorithms(2)});
+  }else if(i === 2){
+    log('Copeland Ranking...');
+    setTimeout(createInformationOnAlgorithm,100,'Copeland', copeland, listOfCandidates, votes, document.getElementById('copeland'), function(){runAllAlgorithms(3)});
+  }else if(i === 3){
+    log('Minimax Ranking...');
+    setTimeout(createInformationOnAlgorithm,100,'Minimax', minimax, listOfCandidates, votes, document.getElementById('minimax'), function(){runAllAlgorithms(4)});
+  }else if(i === 4){
+    log('Borda Ranking...');
+    setTimeout(createInformationOnAlgorithm,100,'Borda', borda, listOfCandidates, votes, document.getElementById('borda'), function(){runAllAlgorithms(5)});
+  }else if( i === 5){
+    log('Kemeny-Young Ranking...');
+    setTimeout(createInformationOnAlgorithm,100,'Kemeny-Young Ranking', kemenyYoung, listOfCandidates, votes, document.getElementById('kemenyYoung'), function(){runAllAlgorithms(6)});
+  }else if(i === 6){
+    log('Baldwin Ranking...');
+    var onEnd = function(){
+      var distances = getDistances(listOfCandidates, votes);
+      document.getElementById('winner').innerHTML = calculateWinner();
+      document.getElementById('svg').innerHTML  = drawDirectedGraph(listOfCandidates, distances);
+      document.getElementById('distances-table').innerHTML = populateDistances(listOfCandidates, distances);
+      setTimeout(closeClapper, 300);
+      setTimeout(shrinkHeader, 800);
+      setTimeout(hideLog,500);
+    }
+    setTimeout(createInformationOnAlgorithm,100,'Baldwin', baldwin, listOfCandidates, votes, document.getElementById('baldwin'), onEnd);
+  }
+}
+
+function populateTable(results){
+  var html = "";
+  if(results && results[0].score){
+    html = "<thead><tr><th>Rank</th><th>Film</th><th>Score</th></tr></thead>";
+    for(var i = 0; i < results.length; i++){
+      if(results[i].rank === 1){
+        html = html + "<tr class='rank1'><td>" + results[i].rank + "</td><td>" + results[i].film + "</td><td>" + results[i].score +"</td></tr>";
+      }else{
+        html = html + "<tr><td>" + results[i].rank + "</td><td>" + results[i].film + "</td><td>" + results[i].score +"</td></tr>";
+      }
+    }
+  }else if(results){
+    html = "<thead><th>Rank</th><th>Film</th></thead>";
+    for(var i = 0; i < results.length; i++){
+      if(results[i].rank === 1){
+        html = html + "<tr class='rank1'><td>" + results[i].rank + "</td><td>" + results[i].film + "</td></tr>";
+      }else{
+        html = html + "<tr><td>" + results[i].rank + "</td><td>" + results[i].film + "</td></tr>";
+      }
+    }
+  }
+  return html;
+}
+
+function calculateWinner(){
+  var numberWon = {};
+  for(var i =0; i < listOfCandidates.length; i++){
+    numberWon[listOfCandidates[i]] = 0;
+  }
+  for(var i = 0; i < results.length; i++){
+    for(var j =0; j < listOfCandidates.length; j++){
+      if(results[i][j].rank === 1){
+        numberWon[results[i][j].film]++;
+      }else{
+        break;
+      }
+    }
+  }
+  var highestScorers = [];
+  var highestScore = 0;
+  for(var i =0; i < listOfCandidates.length; i++){
+    if(numberWon[listOfCandidates[i]] === highestScore){
+      highestScorers.push(listOfCandidates[i]);
+    }else if(numberWon[listOfCandidates[i]] > highestScore){
+      highestScore = numberWon[listOfCandidates[i]];
+      highestScorers = [listOfCandidates[i]];
+    }
+  }
+  // TODO: Check which ones they won;
+  var winner = "It's a draw";
+  if(highestScorers.length === 1){
+    winner = highestScorers[0];
+  }
+  return winner;
+}
+
+function populateDistances(listOfCandidates, distances){
+  var html = "<tbody><tr><th></th>"
+  for(var i = 0; i < listOfCandidates.length; i++){
+    html = html + "<th>" + listOfCandidates[i] + "</th>";
+  }
+  html = html + "</tr>";
+  for(var i = 0; i < listOfCandidates.length; i++){
+    html = html + "<tr><th>" + listOfCandidates[i] + "</th>";
+    for(var j = 0; j < listOfCandidates.length; j++){
+      if(i === j){
+        html = html + "<td class='null'></td>";
+      }else if(distances[i][j] === distances[j][i]){
+          html = html + "<td class='equal'>" + distances[i][j] + "</td>";
+      }else if(distances[i][j] > distances[j][i]){
+          html = html + "<td class='better'>" + distances[i][j] + "</td>";
+      }else{
+          html = html + "<td class='worse'>" + distances[i][j] + "</td>";
+      }
+    }
+    html = html + "</tr>";
+  }
+  html = html + "</tbody>";
+  return html;
+}
+
+function drawDirectedGraph(listOfCandidates, distances){
+  var nodeColors = ['#F44336', '#9C27B0', '#3F51B5', '#009688', '#FF5722', '#795548'];
+  var coldColor = "1B28BF";
+  var warmColor = "BF1B1B";
+
+  var graphRadius = 100;
+  var nodeRadius = 10;
+  var numberOfIndicatorSquares = 10;
+  var size = graphRadius/numberOfIndicatorSquares;
+
+  var svgStart = '<svg id="graph" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="'+ -graphRadius + ' '+ -graphRadius +  ' '+ (2*graphRadius + nodeRadius + 3*size + 25 ) + ' '+ 2*graphRadius + '" style="overflow:visible; height: 300 !important; width: 300 !important;">'
+  var defsHTML = "<defs>";
+  var coordinates = [];
+  for( var i = 0; i < listOfCandidates.length; i++){
+    var x = graphRadius * Math.sin( - i / listOfCandidates.length * 2 *Math.PI + Math.PI);
+    var y = graphRadius * Math.cos( - i / listOfCandidates.length * 2 * Math.PI + Math.PI);
+    coordinates.push({'x': x, 'y': y});
+  }
+  var min = Number.POSITIVE_INFINITY;
+  var max = 0;
+  for( var i =0; i< listOfCandidates.length; i++){
+    for(var j = 0; j < listOfCandidates.length; j++){
+      if(distances[i][j] > distances[j][i]){
+        min = Math.min(min, distances[i][j]);
+        max = Math.max(max, distances[i][j]);
+      }
+    }
+  }
+  for( var i =0; i< listOfCandidates.length; i++){
+    for(var j = 0; j < listOfCandidates.length; j++){
+      if(distances[i][j] < distances[j][i]){
+        var differenceX = coordinates[i].x - coordinates[j].x;
+        var differenceY = coordinates[i].y - coordinates[j].y;
+        var length = Math.sqrt(differenceX*differenceX + differenceY*differenceY);
+        var targetX = coordinates[j].x + differenceX*(length-10)/length;
+        var targetY = coordinates[j].y + differenceY*(length-10)/length;
+        var color = interpolateColors(coldColor, warmColor, (distances[j][i] - min)/(max - min));
+        var newMarker = '<marker id="arrow'+color+'" markerWidth="10" markerHeight="10" refx="6" refy="2" orient="auto" markerUnits="strokeWidth" fill="#'+color+'"><path d="M0,0 L0,4 L6,2 z"/></marker>'
+        defsHTML = defsHTML + newMarker;
+        var line = '<line x1="' + coordinates[j].x + '" y1 = "'+ coordinates[j].y+ '" x2="'+  targetX +'" + y2="'+ targetY +'" stroke-width="2" stroke="#'+color+'" marker-end="url(#arrow'+color+')"/>';
+        svg = svg + line;
+      }
+    }
+  }
+  for(var i = 0; i < listOfCandidates.length; i++){
+      var node = '<circle cx="'+ coordinates[i].x + '" cy="'+ coordinates[i].y + '" r="'+nodeRadius+'" fill="'+nodeColors[i % nodeColors.length] + '"/>';
+      var text = '<text x="'+ coordinates[i].x + '" y="'+ coordinates[i].y + '" text-anchor="middle"  fill="white" font-size="' + nodeRadius * 1.5 + '" style="alignment-baseline:central" font-family="Open Sans">' + String.fromCharCode(65 + i) + '</text>';
+      svg = svg + node + text;
+  }
+  for(var i =0; i < numberOfIndicatorSquares; i++ ){
+
+    var rect = '<rect x="'+ (graphRadius + nodeRadius + size) + '" y="'+(-graphRadius + i*size*2)+'" width="'+size+'" height="'+size+'" fill="#'+interpolateColors(coldColor, warmColor, 1 - i/numberOfIndicatorSquares)+'"/>';
+    var text = '<text x="'+(graphRadius + nodeRadius + 3 * size) + '" y="'+(-graphRadius + i*size*2 + size/2)+ '" font-size="' + size + '" style="alignment-baseline:central" font-family="Open Sans">' + Math.round((max - (max - min)*i/numberOfIndicatorSquares)) + '</text>';
+    svg = svg + rect + text;
+
+  }
+  defsHTML = defsHTML + '</defs>';
+  return svgStart + defsHTML + svg + '</svg>';
+}
+
+function interpolateColors(hex1, hex2, t){
+  var rgb1 = hexToRgb(hex1);
+  var rgb2 = hexToRgb(hex2);
+  return rgbToHex(Math.round((1-t)*rgb1.r + t*rgb2.r), Math.round((1-t)*rgb1.g + t*rgb2.g), Math.round((1-t)*rgb1.b + t*rgb2.b));
+
+}
+
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
+function hexToRgb(hex) {
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+        return r + r + g + g + b + b;
+    });
+
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+</script>
+</body>
+<html>
---
+++ voting-systems.js
@@ -0,0 +1,290 @@
+var listOfCandidates = ['Interstellar','The Lion King','The Dukes of Hazzard','Jurassic World',"Ocean's Eleven"];
+var votes = [{'Jurassic World': 1, 'Interstellar': 1, 'The Lion King': 3, 'The Dukes of Hazzard': 4, "Ocean's Eleven": 5}];
+
+function generateRandomVotes(n){
+  var order = [];
+  for( var i =0; i < listOfCandidates.length; i++){
+    order.push(i);
+  }
+  votes = [];
+  for(var i = 0; i < n; i++){
+    var vote = {};
+    shuffle(order);
+    for(var j = 0; j < listOfCandidates.length; j++){
+      vote[listOfCandidates[j]] = order[j];
+    }
+    votes.push(vote);
+  }
+}
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function getDistances(listOfCandidates, votes) {
+  var distances = [];
+
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    var row = [];
+    for (var j = 0; j < listOfCandidates.length; j++) {
+      var distance = 0;
+      for (var k = 0; k < votes.length; k++) {
+        if (votes[k][listOfCandidates[i]] < votes[k][listOfCandidates[j]]) {
+          distance++;
+        }
+      }
+      row.push(distance);
+    }
+    distances.push(row);
+  }
+  return distances;
+}
+
+function schulze(listOfCandidates, votes) {
+  var distances = getDistances(listOfCandidates, votes);
+  var paths = [];
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    var row = [];
+    for (var j = 0; j < listOfCandidates.length; j++) {
+      if (distances[i][j] > distances[j][i]) {
+        row.push(distances[i][j]);
+      }else {
+        row.push(0);
+      }
+    }
+    paths.push(row);
+  }
+  for (i = 0; i < listOfCandidates.length; i++) {
+    for (j = 0; j < listOfCandidates.length; j++) {
+      if (i !== j) {
+        for (var k = 0; k < listOfCandidates.length; k++) {
+          if (k !== i && k !== j) {
+            paths[j][k] = Math.max(paths[j][k], Math.min(paths[j][i], paths[i][k]));
+          }
+        }
+      }
+    }
+  }
+  var sortFunction = function sort(a, b) {
+    if (paths[a][b] > paths[b][a]) {
+      return -1;
+    }
+    if (paths[b][a] > paths[a][b]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  var mapped = [];
+  for (i = 0; i < listOfCandidates.length; i++) {
+    mapped.push(i);
+  }
+  mapped.sort(sortFunction);
+  var result = mapped.map(function(i) {
+    return {film: listOfCandidates[i], index: i};
+  });
+  result[0].rank = 1;
+  for (i = 1; i < listOfCandidates.length; i++) {
+    if (paths[result[i].index][result[i - 1].index] === paths[result[i - 1].index][result[i].index]) {
+      result[i].rank = result[i - 1].rank;
+    }else {
+      result[i].rank = i + 1;
+    }
+  }
+  return result;
+}
+
+function copeland(listOfCandidates, votes) {
+  var distances = getDistances(listOfCandidates, votes);
+  var scores = [];
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    var score = 0;
+    for (var j = 0; j < listOfCandidates.length; j++) {
+      if (i !== j) {
+        if (distances[i][j] > distances[j][i]) {
+          score++;
+        }else if (distances[i][j] < distances[j][i]) {
+          score--;
+        }
+      }
+    }
+    scores.push({film: listOfCandidates[i], 'score': score, rank: i});
+  }
+  scores.sort(function(a, b) {return b.score - a.score;});
+  scores[0].rank = 1;
+  for (i = 1; i < listOfCandidates.length; i++) {
+    if (scores[i].score === scores[i - 1].score) {
+      scores[i].rank = scores[i - 1].rank;
+    }else {
+      scores[i].rank = i + 1;
+    }
+  }
+  return scores;
+}
+
+function minimax(listOfCandidates, votes) {
+  var distances = getDistances(listOfCandidates, votes);
+  var scores = [];
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    var highestScore = 0;
+    for (var j = 0; j < listOfCandidates.length; j++) {
+      if (distances[j][i] > distances[i][j] && distances[j][i] > highestScore) {
+        highestScore = distances[j][i];
+      }
+    }
+    scores.push({film: listOfCandidates[i], score: highestScore, rank: i});
+  }
+  scores.sort(function(a, b) { return a.score - b.score;});
+  scores[0].rank = 1;
+  for (i = 1; i < listOfCandidates.length; i++) {
+    if (scores[i].score === scores[i - 1].score) {
+      scores[i].rank = scores[i - 1].rank;
+    }else {
+      scores[i].rank = i + 1;
+    }
+  }
+  return scores;
+}
+
+function borda(listOfCandidates, votes) {
+  var scores = [];
+  for (var k = 0; k < listOfCandidates.length; k++) {
+    scores.push({film: listOfCandidates[k], score: 0, rank: k});
+  }
+
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    for (var j = 0; j < votes.length; j++) {
+      scores[i].score = scores[i].score - votes[j][scores[i].film] + 1 + listOfCandidates.length;
+    }
+  }
+  scores.sort(function(a, b) {return b.score - a.score;});
+  scores[0].rank = 1;
+  for (i = 1; i < listOfCandidates.length; i++) {
+    if (scores[i].score === scores[i - 1].score) {
+      scores[i].rank = scores[i - 1].rank;
+    }else {
+      scores[i].rank = i + 1;
+    }
+  }
+  return scores;
+}
+
+function kemenyYoung(listOfCandidates, votes) {
+  var distances = getDistances(listOfCandidates, votes);
+  var currentHighest = -1;
+  var uniqueHighest = true;
+  var rankingOfHighest = [];
+  var array = [];
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    array.push(i);
+  }
+  var permutations = permutator(array);
+  for (i = 0; i < permutations.length; i++) {
+    var score = 0;
+    for (var j = 0; j < listOfCandidates.length - 1; j++) {
+      for (var k = j + 1; k < listOfCandidates.length; k++) {
+        score = score + distances[permutations[i][j]][permutations[i][k]];
+      }
+    }
+    if (score === currentHighest) {
+      uniqueHighest = false;
+    }
+    if (score > currentHighest) {
+      currentHighest = score;
+      rankingOfHighest = permutations[i];
+      uniqueHighest = true;
+    }
+  }
+  if(!uniqueHighest){
+    throw 'Draw for the higest score';
+  }
+  var result = rankingOfHighest.map(function(i, index) {return {film: listOfCandidates[i], rank: index + 1};});
+  return result;
+}
+
+function permutator(inputArr) {
+  var results = [];
+
+  function permute(arr, memo) {
+    var cur;
+    var memo = memo || [];
+
+    for (var i = 0; i < arr.length; i++) {
+      cur = arr.splice(i, 1);
+      if (arr.length === 0) {
+        results.push(memo.concat(cur));
+      }
+      permute(arr.slice(), memo.concat(cur));
+      arr.splice(i, 0, cur[0]);
+    }
+
+    return results;
+  }
+
+  return permute(inputArr);
+}
+
+function baldwin(listOfCandidates, votes) {
+  var currentCandidates = listOfCandidates.slice();
+  var currentVotes = JSON.parse(JSON.stringify(votes));
+  var results = [];
+  while (currentCandidates.length > 0) {
+    var bordaResults = borda(currentCandidates, currentVotes);
+    if(bordaResults.length > 1 && bordaResults[bordaResults.length - 2].score === bordaResults[bordaResults.length - 1].score){
+      throw 'Draw for bottom place';
+    }
+    var toEliminate = bordaResults[bordaResults.length - 1].film;
+    results.push(toEliminate);
+    currentCandidates.splice(currentCandidates.indexOf(toEliminate),1);
+    for (var i = 0; i < currentVotes.length; i++) {
+      var rankOfEliminate = currentVotes[i].toEliminate;
+      for (var j = 0; j < currentCandidates.length; j++) {
+        if (currentVotes[i][currentCandidates[j]] < rankOfEliminate) {
+          currentVotes[i][currentCandidates[j]]--;
+        }
+      }
+    }
+  }
+  results.reverse();
+  results = results.map(function(film, index) {return {'film': film, rank: index + 1};})
+  return results;
+}
+
+function bucklin(listOfCandidates, votes) {
+  var distances = getDistances(listOfCandidates, votes);
+  var voteTotals = [];
+  for (var i = 0; i < listOfCandidates.length; i++){
+
+  }
+}
+
+function rankedPairs(listOfCandidates, votes){
+  var distances = getDistances(listOfCandidates, votes);
+  var currentPaths = [];
+  for (var i = 0; i < listOfCandidates.length; i++) {
+    var row = [];
+    for (var j = 0; j < listOfCandidates.length; j++) {
+      rows.push(0);
+    }
+    currentPaths.push(row);
+  }
+  var pairs = [];
+  for (i = 0; i < listOfCandidates.length; i++) {
+    for (j = i + 1; j < listOfCandidates.length; j++) {
+      if (distances[i][j] > distances[j][i]) {
+
+      }else {
+
+      }
+
+    }
+  }
+}
