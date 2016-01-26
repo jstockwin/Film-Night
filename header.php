e5cb8945977884886347f478cc001476854f4437
jstockwin <jstockwin@gmail.com>
2016-01-26 16:26:47 +0000

    Bug Fix 2

--- header.php
+++ header.php
@@ -1,8 +1,10 @@
 <?php
 if (strpos(gethostname(), 'rpi') !== FALSE){
   $root = $_SERVER['DOCUMENT_ROOT'].'/filmnight/';
+  $root2 = '/filmnight/';
 }else{
   $root = $_SERVER['DOCUMENT_ROOT'].'/';
+  $root2 = '/';
 }
 
 require_once $root.'google.php';
