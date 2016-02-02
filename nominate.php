4792b90b5d671b2a01348ffc6aa06f1b3722e546
jstockwin <jstockwin@gmail.com>
2016-02-02 15:33:16 +0000

    Allow access to nominations page

--- nominate.php
+++ nominate.php
@@ -6,7 +6,7 @@
   <body>
     <?php include 'top-nav.php'; ?>
   <?php endif ?>
-  <?php if(($permission != FALSE && status($root) == "results") || $permission == "admin"): ?>
+  <?php if(($permission != FALSE): ?>
     <script>
     showContent();
 
