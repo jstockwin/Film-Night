c8d619ba02e7a645e226924fd344dad743ad085f
Ben Spiers <ben.spiers22@gmail.com>
2016-02-13 22:55:16 +0000

    For some reason the php is sending /manifest.json Will this fix that?

--- head.php
+++ head.php
@@ -7,7 +7,7 @@ function head($title, $extra='') {
   echo '<title>'.$title.'</title>';
   echo '<link rel="stylesheet" type="text/css" href="styles.css">';
   echo '<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans">';
-  echo '<link rel="manifest" href="manifest.json">';
+  echo '<link rel="manifest" href="./manifest.json">';
   echo '<link rel="icon" type="image/svg+xml" href="assets/icons/favicon.svg" sizes="any">';
   echo '<link rel="icon" type="image/png" href="assets/icons/favicon.png">';
   echo '<meta name="theme-color" content="#607D8B">';
