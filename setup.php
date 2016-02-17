ccedb5a7bcd12fb37e1b5f0af4138801ce980348
jstockwin <jstockwin@gmail.com>
2016-02-17 17:20:50 +0000

    Implement #58
    Closes #58

--- setup.php
+++ setup.php
@@ -2,11 +2,11 @@
 if (strpos(gethostname(), 'rpi') !== FALSE){
   $root = $_SERVER['DOCUMENT_ROOT'].'/filmnight/';
   $root2 = '/filmnight/';
+  $session = "live";
 }else{
   $root = $_SERVER['DOCUMENT_ROOT'].'/';
   $root2 = '/';
+  $session = "dev";
 }
 require_once $root.'functions.php';
-$session = "live";
-include $root.'../override.php';
 ?>
