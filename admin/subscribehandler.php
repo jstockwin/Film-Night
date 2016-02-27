e09c94c0b076e37c945dfc0e6ab86eb231884945
Ben Spiers <ben.spiers22@gmail.com>
2016-02-27 22:32:00 +0000

    Stop subscribe handler printing to error log.

--- admin/subscribehandler.php
+++ admin/subscribehandler.php
@@ -18,14 +18,12 @@ if(!loginCheck($session)){
     $endpoint=$_POST['endpoint'];
     $sql = 'INSERT INTO endpoints VALUES (NULL,"'.$_SESSION['ID'].'","'.$_POST['name'].'","'.$_POST['endpoint'].'");';
     $result = $conn->query($sql);
-    error_log(print_r($endpoint, TRUE));
     $sql = 'SELECT Identifier, Name FROM endpoints where Endpoint="'.$endpoint.'";';
     $result = $conn->query($sql);
     if($result->num_rows > 0){
       $row = $result->fetch_assoc();
       $result = json_encode(array("Identifier" => $row['Identifier'], "Name" => $row['Name']));
     }
-    error_log(print_r($result, TRUE));
     print_r($result);
   }else{
     echo "Error: Nothing recieved";
