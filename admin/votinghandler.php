af32982e6b074f2c35ebfea4b1cd80318f40dd5f
jstockwin <jstockwin@gmail.com>
2016-03-01 20:51:04 +0000

    Voting now works with new db

--- admin/votinghandler.php
+++ admin/votinghandler.php
@@ -15,14 +15,7 @@ if(!loginCheck($session)){
   }
   if(isset($_POST['votes'])){
     if($_POST['votes'] == "WITHDRAW"){
-      $sql2 = "DELETE FROM incomingvotes WHERE ID='".$_SESSION['ID']."';";
-      $result2 = $conn->query($sql2);
-      if($result2==1){
-        echo "successfully removed vote";
-      }else{
-        echo "Error: There was an error submitting to the database: ".$sql2;
-        echo "Returned: ".$result2;
-      }
+      withdrawVotes(getCurrentFilmNight(), $_SESSION['ID']);
     }else{
       $post = file_get_contents('php://input');
       echo $post;
@@ -77,35 +70,7 @@ if(!loginCheck($session)){
 
 
       if($continue){
-        // Check if they have already voted
-        $sql = "SELECT * FROM incomingvotes WHERE ID='".$_SESSION['ID']."'";
-        $result = $conn->query($sql);
-        if ($result->num_rows == 0){
-          // User has not yet voted.
-          $sql2 = "INSERT INTO incomingvotes VALUES ('".$_SESSION['ID']."', '".$vote."');";
-          $result2 = $conn->query($sql2);
-          echo "New Vote: ".$result2;
-          if($result2==1){
-            echo "successfully sumbitted vote";
-          }else{
-            echo "Error: There was an error submitting to the database: ".$sql2;
-            echo "Returned: ".$result2;
-            $_SESSION['ERROR'] = "Error: Failed to submit vote:<br> SQL Call:<br>".$sql."<br>result<br>".$result;
-          }
-
-        }else{
-          //user has already voted.
-          $sql2 = "UPDATE incomingvotes SET Vote='".$vote."' WHERE ID='".$_SESSION['ID']."';";
-          $result2 = $conn->query($sql2);
-          echo "Updated Vote: ".$result2;
-          if($result2==1){
-            echo "successfully sumbitted vote";
-          }else{
-            echo "Error: There was an error submitting to the database: ".$sql2;
-            echo "Returned: ".$result2;
-            $_SESSION['ERROR'] = "Error: Failed to submit vote:<br> SQL Call:<br>".$sql."<br>result<br>".$result;
-          }
-        }
+        addVote(getCurrentFilmNight() ,$_SESSION['ID'], $jsonVote);
       }
     }
   }
